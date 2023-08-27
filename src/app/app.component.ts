9be6b7ba6377b837ed2b118434f43112fed3786b
Aleksuo <aleksi.suoranta@gmail.com>
2023-08-27 15:04:31 +0300

    Add and access Canvas in AppComponent

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ViewChild, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'angular-ray-tracing';
+export class AppComponent implements AfterViewInit {
+
+  @ViewChild('canvasElement', {static: false}) canvasElement: ElementRef<HTMLCanvasElement> = {} as ElementRef<HTMLCanvasElement>;
+
+  context: CanvasRenderingContext2D | null = null;
+
+  ngAfterViewInit(): void {
+    this.context = this.canvasElement.nativeElement.getContext('2d');
+    if(this.context) {
+      this.context.fillStyle = 'green';
+      this.context.fillRect(0, 0, 100, 100);
+    }
+  }
 }
