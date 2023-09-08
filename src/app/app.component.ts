a8def0f884e2309d4aaa48cbfccae07fb74a532e
Aleksuo <aleksi.suoranta@gmail.com>
2023-09-08 21:03:32 +0300

    Render using imgdata and putImageData

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,11 +11,26 @@ export class AppComponent implements AfterViewInit {
 
   context: CanvasRenderingContext2D | null = null;
 
+  image_width = 256;
+  image_height = 256;
+
   ngAfterViewInit(): void {
     this.context = this.canvasElement.nativeElement.getContext('2d');
     if(this.context) {
-      this.context.fillStyle = 'green';
-      this.context.fillRect(0, 0, 100, 100);
+      this.context.canvas.width = this.image_width;
+      this.context.canvas.height = this.image_height;
+
+      const imgData = new ImageData(this.image_width, this.image_height);
+      for(let i = 0; i < this.image_height; i++) {
+        for(let j = 0; j < this.image_width; j++) {
+          const index = (i * this.image_width + j) * 4;
+          imgData.data[index] = 255;
+          imgData.data[index + 1] = 0;
+          imgData.data[index + 2] = 0;
+          imgData.data[index + 3] = 255;
+        }
+      }
+      this.context.putImageData(imgData, 0, 0);
     }
   }
 }
