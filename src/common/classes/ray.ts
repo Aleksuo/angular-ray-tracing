2998cc4b7877d6c26a60b2f66193edb95e1be459
Aleksuo <aleksi.suoranta@gmail.com>
2023-09-09 12:34:37 +0300

    Create the Ray-class

---
+++ src/common/classes/ray.ts
@@ -0,0 +1,18 @@
+import { Point3 } from '../types/vec3.types';
+import { Vec3 } from './vec3';
+import { Vec3Utils } from '../utils/vec3.utils';
+
+export class Ray {
+
+    origin: Point3;
+    direction: Vec3;
+
+    constructor(origin: Point3, direction: Vec3) {
+        this.origin = origin;
+        this.direction = direction;
+    }
+
+    at(t: number): Point3 {
+        return Vec3Utils.add(this.origin, Vec3Utils.multiply(t, this.direction));
+    }
+}
