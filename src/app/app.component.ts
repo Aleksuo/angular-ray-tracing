5df2bb7a94417d3e1cfa6ad81e1d833b4b6ee558
Aleksuo <aleksi.suoranta@gmail.com>
2023-09-09 12:48:48 +0300

    Add and run prettier for project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,12 @@ import { AfterViewInit, Component, ViewChild, ElementRef } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent implements AfterViewInit {
-
-  @ViewChild('canvasElement', {static: false}) canvasElement: ElementRef<HTMLCanvasElement> = {} as ElementRef<HTMLCanvasElement>;
+  @ViewChild('canvasElement', { static: false })
+  canvasElement: ElementRef<HTMLCanvasElement> =
+    {} as ElementRef<HTMLCanvasElement>;
 
   context: CanvasRenderingContext2D | null = null;
 
@@ -16,13 +17,13 @@ export class AppComponent implements AfterViewInit {
 
   ngAfterViewInit(): void {
     this.context = this.canvasElement.nativeElement.getContext('2d');
-    if(this.context) {
+    if (this.context) {
       this.context.canvas.width = this.image_width;
       this.context.canvas.height = this.image_height;
 
       const imgData = new ImageData(this.image_width, this.image_height);
-      for(let i = 0; i < this.image_height; i++) {
-        for(let j = 0; j < this.image_width; j++) {
+      for (let i = 0; i < this.image_height; i++) {
+        for (let j = 0; j < this.image_width; j++) {
           const index = (i * this.image_width + j) * 4;
           imgData.data[index] = 255;
           imgData.data[index + 1] = 0;
--- src/common/types/vec3.types.ts
+++ src/common/types/vec3.types.ts
@@ -1,4 +1,4 @@
-import { Vec3 } from "../classes/vec3";
+import { Vec3 } from '../classes/vec3';
 
 export type Color = Vec3;
 export type Point3 = Vec3;
