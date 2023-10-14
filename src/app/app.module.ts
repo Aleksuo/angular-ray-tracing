1ab0fd2e4d773dbc84f4a54f73d5b48882422bae
Aleksi Suoranta <aleksi.suoranta@gmail.com>
2023-10-14 13:25:58 +0300

    Remove router as there is no need for it

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,14 +2,13 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { ReactiveFormsModule } from '@angular/forms';
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgCanvasComponent } from './ng-canvas/ng-canvas.component';
 import { SettingsPanelComponent } from './settings-panel/settings-panel.component';
 
 @NgModule({
   declarations: [AppComponent, NgCanvasComponent, SettingsPanelComponent],
-  imports: [BrowserModule, AppRoutingModule, ReactiveFormsModule],
+  imports: [BrowserModule, ReactiveFormsModule],
   providers: [],
   bootstrap: [AppComponent],
 })
